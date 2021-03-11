7c162891da4bba91d4ff311d322992d9d4f1285d
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-11 18:44:13 +0300

    copy nursinng

---
+++ Citizenship/Resource/Citizenship-Thermo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Citizenship-Thermo-Bridging-Header.h
+//  Citizenship
+//
+//  Created by Andrey Chernyshev on 11.03.2021.
+//
+
+#ifndef Citizenship_Thermo_Bridging_Header_h
+#define Citizenship_Thermo_Bridging_Header_h
+
+#import <RushSDK/RushSDK.h>
+
+#endif /* Citizenship_Thermo_Bridging_Header_h */
