c3214ce392c3d0ad90037c7794c98321775dab89
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-11 17:52:35 +0300

    init

---
+++ Citizenship/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Citizenship
+//
+//  Created by Andrey Chernyshev on 11.03.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
