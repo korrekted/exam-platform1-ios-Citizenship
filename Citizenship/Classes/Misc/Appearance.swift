2e03c583bed35e0cecbf6d7e2e70a196977f4cee
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-19 22:30:01 +0300

    copy

---
+++ Citizenship/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 255, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 65, green: 148, blue: 150)
+    static let mainColorAlpha = UIColor(integralRed: 65, green: 148, blue: 150, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 236, green: 95, blue: 68)
+    
+    static let progress1Color = UIColor(integralRed: 102, green: 102, blue: 102)
+    static let progress2Color = UIColor(integralRed: 65, green: 148, blue: 150)
+    static let progress3Color = UIColor(integralRed: 236, green: 95, blue: 68)
+}
