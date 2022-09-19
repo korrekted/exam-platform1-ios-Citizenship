2fffbb4724fbd9b7518157c24b1b94678b720431
akonst17 <akonst17@gmail.com>
2022-09-19 20:43:34 +0300

    интеграция sdk 2.0; новые урлы; build 32

--- Citizenship/Classes/Misc/GlobalDefinitions.swift
+++ Citizenship/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.quantresear.ch"
+    static let domainUrl = "https://exams.mastrapi.com"
     static let apiKey = "EffblogGOddBYma1"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "gsOIhtCj8lQ8mzXY"
 
     static let applicationTag = "EXAM_CITIZENSHIP"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "gsOIhtCj8lQ8mzXY"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "WYFyNIXlHEKnJvXm"
 
     static let appStoreUrl = "https://apps.apple.com/app/citizen-test-2022/id1556996704"
     static let appleAppID = "1556996704"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
